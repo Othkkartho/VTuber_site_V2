4e2deaf23e8889bd2a844b514bef69e3f80a03ef
qhtkdrma <kimwonkyu15@gmail.com>
2022-09-13 23:04:46 +0900

    Access Token을 위한 핸들러 클래스 선언

---
+++ src/main/java/com/othkkartho/vtuber_site_v2/handler/token/JwtHandler.java
@@ -0,0 +1,54 @@
+package com.othkkartho.vtuber_site_v2.handler.token;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtHandler {
+    private String type = "Bearer ";
+
+    public String createToken(String encodedKey, String subject, long maxAgeSeconds) {
+        Date now = new Date();
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        return type + Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + maxAgeSeconds * 1000L))
+//                .signWith(SignatureAlgorithm.HS256, encodedKey)
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractSubject(String encodedKey, String token) {
+        return parse(encodedKey, token).getBody().getSubject();
+    }
+
+    public boolean validate(String encodedKey, String token) {
+        try {
+            parse(encodedKey, token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    private Jws<Claims> parse(String keys, String token) {
+//        return Jwts.parser()
+//                .setSigningKey(key)
+//                .parseClaimsJws(untype(token));
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build().parseClaimsJws(untype(token));
+    }
+
+    private String untype(String token) {
+        return token.substring(type.length());
+    }
+    // 즐겨찾기 4번째에 설명 있음. 주석의 코드들은 Deprecated이 선언되었기에 대체 코드를 만듬.
+    // 나중에 정확한 키 선언 방법을 공부한 후 코드를 재 변경할 예정
+}
