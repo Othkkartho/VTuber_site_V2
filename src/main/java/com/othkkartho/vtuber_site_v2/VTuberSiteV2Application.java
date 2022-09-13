db018860fb9076c331ee00405397efbfbbbfb9c4
Othkkartho <kimwonkyu15@gmail.com>
2022-09-13 09:59:24 +0900

    Initial commit

---
+++ src/main/java/com/othkkartho/vtuber_site_v2/VTuberSiteV2Application.java
@@ -0,0 +1,13 @@
+package com.othkkartho.vtuber_site_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VTuberSiteV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VTuberSiteV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/othkkartho/vtuber_site_v2/VTuberSiteV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.othkkartho.vtuber_site_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VTuberSiteV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
